4b5be156a39aca1afda11da7a11768d628f17887
Belianin <belyaninwork@gmail.com>
2020-07-21 18:09:34 +0500

    init

---
+++ Codenames/Startup.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Codenames
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(o => o.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
